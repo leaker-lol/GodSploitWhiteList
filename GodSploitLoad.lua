4840f2a9b50dba9688c0f0ac21c3cf9549b93b8b
NotHm_ <127741557+AfgMS@users.noreply.github.com>
2024-02-18 18:56:01 +0700

    Create GodSploitLoad.lua

---
+++ GodSploitLoad.lua
@@ -0,0 +1,29 @@
+repeat task.wait() until game:IsLoaded()
+
+local MainScriptLoadstring = "https://raw.githubusercontent.com/AfgMS/GodSploitWhiteList/main/GodSploitSex.lua"
+local SecurityLoadstring = "https://raw.githubusercontent.com/AlSploit/GodSploit/main/Security"
+
+local function InitMainScript()
+	task.spawn(function()
+		loadstring(game:HttpGet(MainScriptLoadstring))()
+	end)	
+end
+
+local function InitSecurity()
+	task.spawn(function()
+		loadstring(game:HttpGet(SecurityLoadstring))()
+	end)
+end
+
+
+task.spawn(function()
+	task.spawn(function()
+		InitSecurity()
+	end)
+
+	task.wait()
+
+	task.spawn(function()
+		InitMainScript()
+	end)
+end)
